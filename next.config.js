9738b6a09e58a4e03f1a57c90ddb18c1d4a43145
Ahmed Elsayad <ahmedelsayad98@gmail.com>
2022-03-10 21:47:00 +0200

    setup of front-end

---
+++ next.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+	reactStrictMode: true,
+	images: {
+		domains: [
+			"cdn.shopify.com",
+			"images.canadagoose.com",
+			"images-na.ssl-images-amazon.com",
+			"store.storeimages.cdn-apple.com"
+		],
+	},
+};
